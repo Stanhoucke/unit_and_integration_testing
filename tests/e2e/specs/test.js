78570925134ff83ec7f84a64b41610c44143923e
Stanhoucke <stanhoucke@gmail.com>
2021-01-29 18:19:55 +0000

    passes arithmetic tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -15,4 +15,40 @@ describe('calculator', () => {
     cy.get('#number6').click();
     cy.get('.display').should('contain', '36');
   });
+
+  // Arithmetic operations
+  it('should update the display with result of adding', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '7');
+  });
+
+  it('should update the display with result of subtracting', () => {
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '5');
+  });
+
+  it('should update the display with result of multiplying', () => {
+    cy.get('#number4').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '12');
+  });
+
+  it('should update the display with result of dividing', () => {
+    cy.get('#number2').click();
+    cy.get('#number4').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number8').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '3');
+  });
+
 })
